469b077f25ef6df41b68388e7c25b1a40203d413
Juraci Vieira <juraci.vieira@gmail.com>
2016-05-08 19:38:34 -0300

    Adding notes feature

---
+++ app/components/edit-note.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    actions: {
+        saveNote() {
+            this.get('note').save();
+        },
+        clouseNote() {
+            this.sendAction('close');
+        }
+    }
+});
